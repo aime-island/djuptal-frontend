607c281272add5ce00c85a20038d409f3db72ef3
ollijonsson <olli.jonsson@gmail.com>
2019-11-08 23:58:29 +0000

    path

--- src/functions/websockets.js
+++ src/functions/websockets.js
@@ -1,6 +1,6 @@
 export const createSocket = (path) => {
     return new Promise((resolve, reject) => {
-        let socket = new WebSocket("ws://10.178.107.100:9000/" + path)
+        let socket = new WebSocket("wss://deepspeech.olafur.io/api/" + path)
         //let socket = new WebSocket("ws://127.0.0.1:9000/" + path)
 
         socket.binaryType = "arraybuffer";
